90955ecd6d94b25d002fe1b0136e665a16fb6b7b
KshitizSharma11 <kshitizsharma.00@gmail.com>
2026-06-06 13:16:58 +0530

    added classvalidators and logger through pino with dockerization of reservations app with docker compose

--- apps/reservations/src/dto/create-reservation.dto.ts
+++ apps/reservations/src/dto/create-reservation.dto.ts
@@ -1,8 +1,24 @@
+import { Type } from 'class-transformer';
+import { IsDate, IsString, IsNotEmpty } from 'class-validator';
+
 export class CreateReservationDto {
-  timestamp: Date;
-  startDate: Date;
-  endDate: Date;
-  userId: string;
-  placeId: string;
-  invoiceId: string;
+  @IsDate()
+  timestamp: Date | undefined;
+
+  @IsDate()
+  @Type(() => Date)
+  startDate!: Date;
+
+  @IsDate()
+  @Type(() => Date)
+  endDate!: Date;
+
+  userId!: string;
+  @IsString()
+  @IsNotEmpty()
+  placeId!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  invoiceId!: string;
 }
